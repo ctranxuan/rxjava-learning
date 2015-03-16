17f5c7331d884afd3f47687549e7b4332b4db65a
ctranxuan <ctranxuan@gmail.com>
2015-03-16 21:25:47 +0100

    chore(rxjava): init the project

---
+++ src/main/java/org/ws13/learning/rxjava/BasicApp.java
@@ -0,0 +1,10 @@
+package org.ws13.learning.rxjava;
+
+/**
+ * @author ctranxuan
+ */
+public class BasicApp {
+    public static void main(String[] args) {
+        System.out.println("BasicApp.main");
+    }
+}
